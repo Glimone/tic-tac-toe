8ae6e7fd40837e7a152021d0e38bb2ad6617c2f0
root <root@DESKTOP-JPOJC7K>
2024-01-09 00:10:37 -0300

    Explicação (Incompleta) de index.js, que deve ser intocável.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//Esse arquivo não será modificado, porém, ele é usado para conectar nesse caso o App.js e o browser. Ele é feito em ECMAscript.
+
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
@@ -10,3 +12,11 @@ root.render(
     <App />
   </StrictMode>
 );
+
+/*
+LINHA 1
+import - Serve para importar módulos e definições de outros arquivos (Está nas outras linhas tbm.)
+StrickMode - Componente que destaca problemas no código.
+O resto, ou seja 'React' e "from react", basicamente importa o módulo react do pacote "react".
+
+*/
