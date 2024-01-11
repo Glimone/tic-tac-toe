a7b620f63eb956b64aa266a20fe435feb70dbf9c
root <root@DESKTOP-JPOJC7K>
2024-01-11 15:25:02 -0300

    Retirada de props value e adição de useState. Criação de array com value e setValue, para receber e alterar valor. Inicio de useState com null.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-//Criamos uma função chamada "Square", aplicamos o retorno o button. Chamamos a função dentro da function Board usando JSX ( <Square /> ). Com isso, é possível realizarmos a alteração de  único valor, assim alterando todos os squares.
-function Square({ value }) {
-                  //value nesse caso é um props, retornando a function pai, Board.
+import { useState } from "react"; //Importando useState de React, que lembra estados.
 
+//Criamos uma função chamada "Square", aplicamos o retorno o button. Chamamos a função dentro da function Board usando JSX ( <Square /> ). Com isso, é possível realizarmos a alteração de  único valor, assim alterando todos os squares.
+function Square() {
+  const [value, setValue] = useState(null); //Definição de null pois o estado inicia em nulo.
+        //value armazena o valor, setValue altera.
   function handleClick() { //Criação de função que informa o momento do click.
-    console.log ('Clicked:')
+    setValue ('X');
   }
   return (                     //Definição de onClick tendo como valor a function handleClick(Ver no console), acima, tudo dentro do return de Square.
     <button className="square" onClick={handleClick}> {value} </button>
@@ -17,22 +19,22 @@ export default function Board() {
   return (
     <> {/*Os componentes react só podem retornar  elemento JSX, ou seja, não podem ser vários buttons. Por isso, usamos "<>" e "</>", para agrupar vários elementos e para que considere como apenas um. */}
       <div className="board-row"> 
-        <Square value="1" /> {/*value aqui, é a props retornada. */}
-        <Square value="2" /> 
-        <Square value="3" />
+        <Square /> 
+        <Square /> 
+        <Square />
       </div>
  
  {/*classname - Método JSX de aplicar uma classe CSS no <button>. A classe é a square, que pode ser vista no arquivo CSS. */}
       <div className="board-row"> 
-        <Square value="4" />
-        <Square value="5" /> {/*<Square é uma var que foi renderizada pelo JSX. */}
-        <Square value="6" />
+        <Square />
+        <Square /> {/*<Square é uma var que foi renderizada pelo JSX. */}
+        <Square />
       </div>
 
       <div className="board-row"> 
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       
     </>
